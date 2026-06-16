d54da42a5173c0e7de2420746fb47d9f64cee521
Aqsa Saher <aqsasaher5995@gmail.com>
2026-06-16 22:05:06 +0100

    fix: add /api to all property endpoints

--- src/services/property.service.js
+++ src/services/property.service.js
@@ -3,37 +3,37 @@ import api from './api';
 export const propertyService = {
   getAllProperties: async (filters = {}) => {
     const params = new URLSearchParams(filters).toString();
-    const response = await api.get(`/properties${params ? `?${params}` : ''}`);
+    const response = await api.get(`/api/properties${params ? `?${params}` : ''}`);
     return response.data;
   },
 
   getPropertyById: async (id) => {
-    const response = await api.get(`/properties/${id}`);
+    const response = await api.get(`/api/properties/${id}`);
     return response.data;
   },
 
   createProperty: async (propertyData) => {
-    const response = await api.post('/properties', propertyData);
+    const response = await api.post('/api/properties', propertyData);
     return response.data;
   },
 
   updateProperty: async (id, propertyData) => {
-    const response = await api.put(`/properties/${id}`, propertyData);
+    const response = await api.put(`/api/properties/${id}`, propertyData);
     return response.data;
   },
 
   deleteProperty: async (id) => {
-    const response = await api.delete(`/properties/${id}`);
+    const response = await api.delete(`/api/properties/${id}`);
     return response.data;
   },
 
   getMyProperties: async () => {
-    const response = await api.get('/properties/my/properties');
+    const response = await api.get('/api/properties/my/properties');
     return response.data;
   },
 
   getPropertyStats: async () => {
-    const response = await api.get('/properties/my/stats');
+    const response = await api.get('/api/properties/my/stats');
     return response.data;
   },
 };
