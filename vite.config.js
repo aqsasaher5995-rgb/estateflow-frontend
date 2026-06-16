0d1c1aea28bdccaa70a077f801305c42ef4d6b39
Aqsa Saher <aqsasaher5995@gmail.com>
2026-06-16 23:13:43 +0100

    fix: add proxy for API in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://estateflow-backend-djex6wllj-aqsasaher5995-rgbs-projects.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
+});
